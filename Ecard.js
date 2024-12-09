dc0ac06f5048d25d02e50e3ac458a0393e225370
Lxs-Lncn <alexislancian0599@gmail.com>
2024-12-09 15:56:40 +0800

    Add files via upload

---
+++ Ecard.js
@@ -0,0 +1,8 @@
+document.getElementById('message-btn').addEventListener('click', function() {
+    const message = document.getElementById('message');
+    if (message.classList.contains('hidden')) {
+        message.classList.remove('hidden');
+    } else {
+        message.classList.add('hidden');
+    }
+});
